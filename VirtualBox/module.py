597d2f06c428c88e97072ea78418140f84777972
Peter Howe <pnhowe@gmail.com>
2017-03-04 07:49:47 -0700

    added rollback to  virtualbox  create

--- VirtualBox/module.py
+++ VirtualBox/module.py
@@ -4,32 +4,43 @@ class create( ExternalFunction ):
   def __init__( self, *args, **kwargs ):
     super().__init__( *args, **kwargs )
     self.done = False
-    self.created = None
+    self.in_rollback = False
 
   @property
   def ready( self ):
     if self.done is True:
       return True
     else:
-      return 'Waiting for VM Creation'
+      if self.in_rollback:
+        return 'Waiting for VM Rollback'
+      else:
+        return 'Waiting for VM Creation'
 
   @property
   def value( self ):
-    return self.created
+    return None
 
   def toSubcontractor( self ):
-    return ( 'create', { 'disk_list': [ 10 ], 'interface_list': [ 'eth0' ] } )
+    if self.in_rollback:
+      return ( 'create_rollback', { 'name': 'thetesting', 'disk_list': [ { 'name': 'sda', 'size': 2 }, { 'name': 'cd', 'file': '/home/peter/Downloads/ubuntu-16.04.2-server-amd64.iso' } ] } )
+    else:
+      return ( 'create', { 'name': 'thetesting', 'disk_list': [ { 'name': 'sda', 'size': 2 }, { 'name': 'cd', 'file': '/home/peter/Downloads/ubuntu-16.04.2-server-amd64.iso' } ], 'interface_list': [ 'eth0' ] } )
 
-  def fromSubcontractor( self, data ):
-    self.done = True
-    self.created = data
+  def fromSubcontractor( self, data ): # TODO: really if these are missing or false, there is a problem
+    if self.in_rollback:
+      self.in_rollback = not data.get( 'rollback_done', False )
+    else:
+      self.done = not data.get( 'done', False )
+
+  def rollback( self ):
+    self.in_rollback =  True
 
   def __getstate__( self ):
-    return ( self.done, self.created )
+    return ( self.done, self.in_rollback )
 
   def __setstate__( self, state ):
     self.done = state[0]
-    self.created = state[1]
+    self.in_rollback = state[1]
 
 
 class destroy( ExternalFunction ):
@@ -49,7 +60,7 @@ class destroy( ExternalFunction ):
     return
 
   def toSubcontractor( self ):
-    return ( 'destroy', None )
+    return ( 'destroy', { 'name': 'thetesting' } )
 
   def fromSubcontractor( self, data ):
     self.done = True
