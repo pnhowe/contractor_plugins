12e0a35fa17e9299d01c9c20fca6907e85e95b78
Peter Howe <pnhowe@gmail.com>
2017-06-07 19:47:23 -0600

    changes to set mac

--- VirtualBox/module.py
+++ VirtualBox/module.py
@@ -16,21 +16,7 @@ class create( ExternalFunction ):
     self.uuid = None
     self.interface_list = []
     self.in_rollback = False
-
-    subcontractor_id = 1
-    mac = '080027{0:06x}'.format( ( 4 * subcontractor_id ) )  # leaving room for 4 interfaces per foundation
-    self.vm_paramaters = {
-                           'name': None,
-                           'cpu_count': 1,
-                           'memory_size': 512,  # in Meg
-                           'disk_list': [ { 'name': 'sda', 'size': 5 } ],  # disk size in G
-                           'interface_list': [ { 'type': 'host', 'name': 'vboxnet0', 'mac': mac } ],  # type one of 'host', 'bridge', 'nat', 'internal',  name is name of network to attach to, max 4 interfaces
-                           'boot_order': [ 'net', 'hdd' ]  # list of 'net', 'hdd', 'cd', 'usb'
-                         }
-
-    if False:  # boot from iso instead
-      self.vm_paramaters[ 'disk_list' ].append( { 'name': 'cd', 'file': '/home/peter/Downloads/ubuntu-16.04.2-server-amd64.iso' } )
-      self.vm_paramaters[ 'boot_order' ][0] = 'cd'
+    self.vm_paramaters = {}
 
   @property
   def ready( self ):
@@ -47,6 +33,25 @@ class create( ExternalFunction ):
     return { 'uuid': self.uuid, 'interface_list': self.interface_list }
 
   def setup( self, parms ):
+    try:
+      foundation_id = self.getScriptValue( 'foundation', 'id' )
+    except ValueError as e:
+      raise ParamaterError( '<unknown>', 'Unable to get Foundation Id: {0}'.format( e ) )
+
+    mac = '080027{0:06x}'.format( ( 4 * foundation_id ) )  # leaving room for 4 interfaces per foundation
+    self.vm_paramaters = {
+                           'name': None,
+                           'cpu_count': 1,
+                           'memory_size': 512,  # in Meg
+                           'disk_list': [ { 'name': 'sda', 'size': 5 } ],  # disk size in G
+                           'interface_list': [ { 'type': 'host', 'name': 'vboxnet0', 'mac': mac } ],  # type one of 'host', 'bridge', 'nat', 'internal',  name is name of network to attach to, max 4 interfaces
+                           'boot_order': [ 'net', 'hdd' ]  # list of 'net', 'hdd', 'cd', 'usb'
+                         }
+
+    if False:  # boot from iso instead
+      self.vm_paramaters[ 'disk_list' ].append( { 'name': 'cd', 'file': '/home/peter/Downloads/ubuntu-16.04.2-server-amd64.iso' } )
+      self.vm_paramaters[ 'boot_order' ][0] = 'cd'
+
     try:
       self.vm_paramaters[ 'name' ] = parms[ 'name' ]
     except KeyError:
