fcfd6f5058e745a590f6969b7d253c7eee4b736a
Peter Howe <pnhowe@gmail.com>
2020-04-29 21:11:29 -0600

    adding snmp_get and snmp_set to iputils

--- contractor_plugins/IPUtils/module.py
+++ contractor_plugins/IPUtils/module.py
@@ -14,7 +14,7 @@ class ping( ExternalFunction ):
 
   @property
   def message( self ):
-    if self.result is not None:
+    if self.result is None:
       return 'Waiting for Ping Results'
     else:
       return 'Ping result "{0}"'.format( self.result )
@@ -94,13 +94,158 @@ class waitForPort( ExternalFunction ):
     self.curent_state = state[2]
 
 
+def _snmpSetup( obj, parms ):
+  try:
+    obj.host = parms[ 'host' ]
+  except KeyError:
+    raise ParamaterError( 'host', 'required' )
+
+  if obj.host is None:
+    raise ParamaterError( 'host', 'is None' )
+
+  try:
+    obj.protocol = parms[ 'protocol' ]
+    if obj.protocol not in ( 'SNMPv1', 'SNMPv2c', 'SNMPv3' ):
+      raise ParamaterError( 'protocol', 'invalid' )
+  except KeyError:
+    obj.protocol = 'SNMPv2c'
+
+  creds = {}
+  if obj.protocol == 'SNMPv3':
+    try:
+      creds[ 'user' ] = parms[ 'user' ]
+    except KeyError:
+      raise ParamaterError( 'user', 'required' )
+
+    if creds[ 'user' ] is None:
+      raise ParamaterError( 'user', 'is None' )
+
+    for name in ( 'auth_key', 'priv_key' ):
+      try:
+        creds[ name ] = parms[ name ]
+      except KeyError:
+        pass
+
+  else:
+    try:
+      creds[ 'community' ] = parms[ 'community' ]
+    except KeyError:
+      raise ParamaterError( 'community', 'required' )
+
+    if creds[ 'community' ] is None:
+      raise ParamaterError( 'community', 'is None' )
+
+  obj.creds = creds
+
+  try:
+    obj.oid = parms[ 'oid' ]
+  except KeyError:
+    raise ParamaterError( 'oid', 'required' )
+
+  if obj.oid is None:
+    raise ParamaterError( 'oid', 'is None' )
+
+
+class snmpGet( ExternalFunction ):
+  def __init__( self, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self.host = None
+    self.protocol = None
+    self.creds = {}
+    self.oid = None
+    self.result = None
+
+  @property
+  def done( self ):
+    return self.result is not None
+
+  @property
+  def message( self ):
+    if self.result is None:
+      return 'Waiting for SNMP Get'
+    else:
+      return 'SNMP Get Complete'
+
+  @property
+  def value( self ):
+    return self.result
+
+  def setup( self, parms ):
+    _snmpSetup( self, parms )
+
+  def toSubcontractor( self ):
+    return ( 'snmp_get', { 'connection': { 'host': self.host, 'protocol': self.protocol, 'creds': self.creds }, 'oid': self.oid } )
+
+  def fromSubcontractor( self, data ):
+    self.result = data[ 'value' ]
+
+  def __getstate__( self ):
+    return ( self.host, self.protocol, self.creds, self.oid, self.result )
+
+  def __setstate__( self, state ):
+    self.host = state[0]
+    self.protocol = state[1]
+    self.creds = state[2]
+    self.oid = state[3]
+    self.result = state[4]
+
+
+class snmpSet( ExternalFunction ):
+  def __init__( self, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self.host = None
+    self.protocol = None
+    self.creds = {}
+    self.oid = None
+    self.new_value = None
+    self.complete = None
+
+  @property
+  def done( self ):
+    return self.complete is True
+
+  @property
+  def message( self ):
+    if self.complete is not True:
+      return 'Waiting for SNMP Get'
+    else:
+      return 'SNMP Set Complete'
+
+  def setup( self, parms ):
+    _snmpSetup( self, parms )
+
+    try:
+      self.new_value = parms[ 'value' ]
+    except KeyError:
+      raise ParamaterError( 'value', 'required' )
+
+  def toSubcontractor( self ):
+    return ( 'snmp_set', { 'connection': { 'host': self.host, 'protocol': self.protocol, 'creds': self.creds }, 'oid': self.oid, 'value': self.new_value } )
+
+  def fromSubcontractor( self, data ):
+    self.complete = data.get( 'done', False )
+
+  def __getstate__( self ):
+    return ( self.host, self.protocol, self.creds, self.oid, self.new_value, self.complete )
+
+  def __setstate__( self, state ):
+    self.host = state[0]
+    self.protocol = state[1]
+    self.creds = state[2]
+    self.oid = state[3]
+    self.new_value = state[4]
+    self.complete = state[5]
+
+
 # plugin exports
 
 TSCRIPT_NAME = 'iputils'
 
 TSCRIPT_FUNCTIONS = {
                       'ping': ping,
-                      'wait_for_port': waitForPort
+                      'wait_for_port': waitForPort,
+                      'snmp_get': snmpGet,
+                      'snmp_set': snmpSet
                     }
 
 TSCRIPT_VALUES = {
