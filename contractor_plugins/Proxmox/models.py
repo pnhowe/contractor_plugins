2147627e615263d048dbe44ccd8c0169518c8762
Peter Howe <pnhowe@gmail.com>
2020-05-05 19:51:48 -0600

    initial proxmox

---
+++ contractor_plugins/Proxmox/models.py
@@ -0,0 +1,178 @@
+import random
+from django.db import models
+from django.core.exceptions import ValidationError
+
+from cinp.orm_django import DjangoCInP as CInP
+
+from contractor.Site.models import Site
+from contractor.Building.models import Foundation, Complex, Structure, FOUNDATION_SUBCLASS_LIST, COMPLEX_SUBCLASS_LIST
+from contractor.Foreman.lib import RUNNER_MODULE_LIST
+from contractor.BluePrint.models import FoundationBluePrint
+from contractor.lib.config import getConfig, mergeValues
+
+from contractor_plugins.Proxmox.module import set_power, power_state, wait_for_poweroff, destroy, get_interface_map, set_interface_macs
+
+cinp = CInP( 'Proxmox', '0.1' )
+
+FOUNDATION_SUBCLASS_LIST.append( 'proxmoxfoundation' )
+COMPLEX_SUBCLASS_LIST.append( 'proxmoxcomplex' )
+RUNNER_MODULE_LIST.append( 'contractor_plugins.Proxmox.module' )
+
+
+@cinp.model( property_list=( 'state', 'type' ) )
+class ProxmoxComplex( Complex ):
+  proxmox_host = models.OneToOneField( Structure, help_text='set to Proxmox host, or Proxmox primary/UI host' )
+  proxmox_username = models.CharField( max_length=50 )
+  proxmox_password = models.CharField( max_length=50 )
+
+  @property
+  def subclass( self ):
+    return self
+
+  @property
+  def state( self ):
+    if self.proxmox_host.state != 'built':
+      return 'planned'
+
+    return super().state
+
+  @property
+  def type( self ):
+    return 'Proxmox'
+
+  @property
+  def connection_paramaters( self ):
+    if self.proxmox_password == '_VAULT_':
+      creds = self.proxmox_username
+
+    else:
+      creds = {
+                'username': self.proxmox_username,
+                'password': self.proxmox_password
+              }
+
+    return {
+              'host': self.proxmox_host.primary_address.ip_address,
+              'credentials': creds
+            }
+
+  def newFoundation( self, hostname ):
+    foundation = ProxmoxFoundation( site=self.site, blueprint=FoundationBluePrint.objects.get( pk='proxmox-vm-base' ), locator=hostname )
+    foundation.proxmox_complex = self
+    foundation.full_clean()
+    foundation.save()
+
+    return foundation
+
+  @cinp.check_auth()
+  @staticmethod
+  def checkAuth( user, method, id_list, action=None ):
+    return True
+
+  def clean( self, *args, **kwargs ):
+    super().clean( *args, **kwargs )
+    errors = {}
+
+    if errors:
+      raise ValidationError( errors )
+
+  def __str__( self ):
+    return 'ProxmoxComplex {0}'.format( self.pk )
+
+
+def _vmSpec( foundation ):
+  result = {}
+
+  structure_config = getConfig( foundation.structure )
+  structure_config = mergeValues( structure_config )
+
+  result[ 'vmid' ] = foundation.proxmox_vmid
+  result[ 'core_count' ] = structure_config.get( 'cpu_count', 1 )
+  result[ 'memory_size' ] = structure_config.get( 'memory_size', 1024 )
+  # result[ 'swap_size' ] = structure_config.get( 'swap_size', 1024 ) # for lxc
+  result[ 'disk_size' ] = structure_config.get( 'disk_size', 10 )
+  result[ 'type' ] = 'qemu'  # only support quemu right now
+
+  return result
+
+
+@cinp.model( property_list=( 'state', 'type', 'class_list' ), read_only_list=( 'vcenter_uuid', ) )
+class ProxmoxFoundation( Foundation ):
+  proxmox_complex = models.ForeignKey( ProxmoxComplex, on_delete=models.PROTECT )
+  proxmox_vmid = models.IntegerField( blank=True, null=True )  # not going to do unique, there could be lots of proxmox instances
+
+  @staticmethod
+  def getTscriptValues( write_mode=False ):  # locator is handled seperatly
+    result = super( ProxmoxFoundation, ProxmoxFoundation ).getTscriptValues( write_mode )
+
+    result[ 'proxmox_complex' ] = ( lambda foundation: foundation.proxmox_complex, None )
+    result[ 'proxmox_vmid' ] = ( lambda foundation: foundation.proxmox_vmid, None )
+    result[ 'proxmox_vmspec'] = ( lambda foundation: _vmSpec( foundation ), None )
+
+    return result
+
+  @staticmethod
+  def getTscriptFunctions():
+    result = super( ProxmoxFoundation, ProxmoxFoundation ).getTscriptFunctions()
+    result[ 'power_on' ] = lambda foundation: ( 'proxmox', set_power( foundation, 'on' ) )
+    result[ 'power_off' ] = lambda foundation: ( 'proxmox', set_power( foundation, 'off' ) )
+    result[ 'power_state' ] = lambda foundation: ( 'proxmox', power_state( foundation ) )
+    result[ 'wait_for_poweroff' ] = lambda foundation: ( 'proxmox', wait_for_poweroff( foundation ) )
+    result[ 'destroy' ] = lambda foundation: ( 'proxmox', destroy( foundation ) )
+    result[ 'get_interface_map' ] = lambda foundation: ( 'proxmox', get_interface_map( foundation ) )
+    result[ 'set_interface_macs' ] = lambda foundation: set_interface_macs( foundation )
+
+    return result
+
+  def configAttributes( self ):
+    result = super().configAttributes()
+    result.update( { '_proxmox_vmid': self.proxmox_vmid } )
+    result.update( { '_proxmox_complex': self.proxmox_complex.name } )
+
+    return result
+
+  @property
+  def subclass( self ):
+    return self
+
+  @property
+  def type( self ):
+    return 'Proxmox'
+
+  @property
+  def class_list( self ):
+    return [ 'VM', 'Proxmox' ]
+
+  @property
+  def complex( self ):
+    return self.vcenter_complex
+
+  @cinp.list_filter( name='site', paramater_type_list=[ { 'type': 'Model', 'model': Site } ] )
+  @staticmethod
+  def filter_site( site ):
+    return ProxmoxFoundation.objects.filter( site=site )
+
+  @cinp.check_auth()
+  @staticmethod
+  def checkAuth( user, method, id_list, action=None ):
+    return True
+
+  def clean( self, *args, **kwargs ):
+    super().clean( *args, **kwargs )
+    errors = {}
+
+    if not self.proxmox_vmid:
+      self.proxmox_vmid = random.randint( 100, 999999999 )  # found this by trial and error
+      # TODO: make sure this randomally generated number isn't in use for this complex
+
+    if self.proxmox_vmid < 100:
+      errors[ 'proxmox_vmid' ] = 'Min value is 100'
+
+    if errors:
+      raise ValidationError( errors )
+
+  class Meta:
+    unique_together = ( ( 'proxmox_complex', 'proxmox_vmid' ), )
+
+  def __str__( self ):
+    return 'ProxmoxFoundation {0}'.format( self.pk )
---
+++ contractor_plugins/Proxmox/module.py
@@ -0,0 +1,408 @@
+import re
+
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+
+from contractor.tscript.runner import ExternalFunction, ParamaterError, Pause
+
+NAME_REGEX = re.compile( '^[a-zA-Z][a-zA-Z0-9\.\-_]*$' )
+MAX_POWER_SET_ATTEMPTS = 5
+
+
+# exported functions
+class create( ExternalFunction ):
+  def __init__( self, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self.complete = None
+    self.connection_paramaters = {}
+    self.vm_paramaters = {}
+
+  @property
+  def done( self ):
+    return self.complete is True
+
+  @property
+  def message( self ):
+    if self.complete is True:
+      return 'VM Created'
+    else:
+      return 'Waiting for VM Creation'
+
+  def setup( self, parms ):
+    try:
+      foundation = self.getScriptValue( 'foundation', 'foundation' )
+    except ValueError as e:
+      raise ParamaterError( '<internal>', 'Unable to get Foundation: {0}'.format( e ) )
+
+    try:
+      proxmox_complex = self.getScriptValue( 'foundation', 'proxmox_complex' )
+    except ValueError as e:
+      raise ParamaterError( '<internal>', 'Unable to get Foundation proxmox_complex: {0}'.format( e ) )
+
+    self.connection_paramaters = proxmox_complex.connection_paramaters
+
+    self.vm_paramaters = {}
+
+    try:
+      self.vm_paramaters[ 'name' ] = self.getScriptValue( 'foundation', 'locator' )
+    except KeyError as e:
+      raise ParamaterError( '<internal>', 'Unable to get Foundation Locator: {0}'.format( e ) )
+
+    if not NAME_REGEX.match( self.vm_paramaters[ 'name' ] ):
+      raise ParamaterError( 'invalid name' )
+
+    try:
+      vm_spec = parms[ 'vm_spec' ]
+    except KeyError:
+      raise ParamaterError( 'vm_spec', 'required' )
+
+    if not isinstance( vm_spec, dict ):
+      raise ParamaterError( 'vm_spec', 'must be a dict' )
+
+    for key in ( 'node', ):
+      try:
+        self.vm_paramaters[ key ] = parms[ key ]
+      except KeyError:
+        raise ParamaterError( key, 'required' )
+
+    for key in ( 'core_count', 'memory_size' ):  # , 'swap_size' ): for lxc
+      try:
+        self.vm_paramaters[ key ] = int( vm_spec[ key ] )
+      except KeyError:
+        raise ParamaterError( 'vm_spec.{0}'.format( key ), 'required' )
+      except ( ValueError, TypeError ):
+        raise ParamaterError( 'vm_spec.{0}'.format( key ), 'must be an integer' )
+
+    if self.vm_paramaters[ 'core_count' ] > 64 or self.vm_paramaters[ 'cpu_count' ] < 1:
+      raise ParamaterError( 'core_count', 'must be from 1 to 64')
+    if self.vm_paramaters[ 'memory_size' ] > 1048510 or self.vm_paramaters[ 'memory_size' ] < 512:  # in MB
+      raise ParamaterError( 'memory_size', 'must be from 512 to 1048510' )
+    # if self.vm_paramaters[ 'swap_size' ] > 1048510 or self.vm_paramaters[ 'swap_size' ] < 512:  # in MB  for lxc
+    #   raise ParamaterError( 'swap_size', 'must be from 512 to 1048510' )
+
+    interface_type = 'virtio'
+
+    interface_list = []
+    for interface in foundation.networkinterface_set.all().order_by( 'physical_location' ):
+      interface_list.append( { 'name': interface.name, 'physical_location': interface.physical_location, 'network': interface.network.name, 'type': interface_type } )
+
+    self.vm_paramaters[ 'disk_list' ] = [ { 'size': vm_spec.get( 'disk_size', 10 ), 'name': 'sda', 'type': vm_spec.get( 'disk_provisioning', 'thin' ) } ]  # disk size in G, see _createDisk in subcontractor_plugsin/vcenter/lib.py
+    self.vm_paramaters[ 'interface_list' ] = interface_list
+    self.vm_paramaters[ 'boot_order' ] = [ 'net', 'hdd' ]  # list of 'net', 'hdd', 'cd'
+
+  def toSubcontractor( self ):
+    return ( 'create', { 'connection': self.connection_paramaters, 'vm': self.vm_paramaters } )
+
+  def fromSubcontractor( self, data ):
+    self.complete = True
+
+  def __getstate__( self ):
+    return ( self.connection_paramaters, self.vm_paramaters, self.complete )
+
+  def __setstate__( self, state ):
+    self.connection_paramaters = state[0]
+    self.vm_paramaters = state[1]
+    self.complete = state[2]
+
+
+class node_list( ExternalFunction ):
+  def __init__( self, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self.node_list = None
+    self.connection_paramaters = {}
+    self.min_memory = None
+    self.cpu_scaler = None
+    self.memory_scaler = None
+
+  @property
+  def done( self ):
+    return self.node_list is not None
+
+  @property
+  def message( self ):
+    if self.node_list is not None:
+      return 'Node List Length: "{0}"'.format( len( self.node_list ) )
+    else:
+      return 'Waiting for Node List'
+
+  @property
+  def value( self ):
+    return self.node_list
+
+  def setup( self, parms ):
+    try:
+      proxmox_complex = self.getScriptValue( 'foundation', 'proxmox_complex' )
+    except ValueError as e:
+      raise ParamaterError( '<internal>', 'Unable to get Foundation proxmox_complex: {0}'.format( e ) )
+
+    self.connection_paramaters = proxmox_complex.connection_paramaters
+
+    for key in ( 'min_memory', 'min_cores' ):  # memory in MB
+      try:
+        setattr( self, key, int( parms[ key ] ) )
+      except KeyError:
+        raise ParamaterError( key, 'required' )
+      except ( ValueError, TypeError ):
+        raise ParamaterError( key, 'must be an integer' )
+
+    for key in ( 'cpu_scaler', 'memory_scaler' ):
+      try:
+        setattr( self, key, int( parms.get( key, 1 ) ) )
+      except ( ValueError, TypeError ):
+        raise ParamaterError( key, 'must be an integer' )
+
+  def toSubcontractor( self ):
+    return ( 'node_list', { 'connection': self.connection_paramaters, 'min_memory': self.min_memory, 'min_cpu': self.min_cpu, 'cpu_scaler': self.cpu_scaler, 'memory_scaler': self.memory_scaler } )
+
+  def fromSubcontractor( self, data ):
+    self.node_list = data[ 'node_list' ]
+
+  def __getstate__( self ):
+    return ( self.connection_paramaters, self.min_memory, self.min_cpu, self.cpu_scaler, self.memory_scaler, self.node_list )
+
+  def __setstate__( self, state ):
+    self.connection_paramaters = state[0]
+    self.min_memory = state[3]
+    self.min_cpu = state[4]
+    self.cpu_scaler = state[5]
+    self.memory_scaler = state[6]
+    self.node_list = state[7]
+
+
+# other functions used by the proxmox foundation
+class destroy( ExternalFunction ):
+  def __init__( self, foundation, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self.vmid = foundation.proxmox_vmid
+    self.name = foundation.locator
+    self.connection_paramaters = foundation.proxmox_complex.connection_paramaters
+    self.complete = None
+
+  @property
+  def done( self ):
+    return self.complete is True
+
+  @property
+  def message( self ):
+    if self.complete is True:
+      return 'VM Destroyed'
+    else:
+      return 'Waiting for VM Destruction'
+
+  def toSubcontractor( self ):
+    return ( 'destroy', { 'connection': self.connection_paramaters, 'vmid': self.vmid, 'name': self.name } )
+
+  def fromSubcontractor( self, data ):
+    self.complete = True
+
+  def __getstate__( self ):
+    return ( self.connection_paramaters, self.vmid, self.name, self.complete )
+
+  def __setstate__( self, state ):
+    self.connection_paramaters = state[0]
+    self.vmid = state[1]
+    self.name = state[2]
+    self.complete = state[3]
+
+
+class set_power( ExternalFunction ):  # TODO: need a delay after each power command, at least 5 seconds, last ones could possibly be longer
+  def __init__( self, foundation, state, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self.vmid = foundation.proxmox_vmid
+    self.name = foundation.locator
+    self.connection_paramaters = foundation.proxmox_complex.connection_paramaters
+    self.desired_state = state
+    self.curent_state = None
+    self.counter = 0
+
+  def run( self ):
+    if self.desired_state != self.curent_state and self.counter > MAX_POWER_SET_ATTEMPTS:
+      raise Pause( 'To Many Attempts to set power to "{0}", curently "{1}"'.format( self.desired_state, self.curent_state ) )
+
+  @property
+  def done( self ):
+    return self.desired_state == self.curent_state
+
+  @property
+  def message( self ):
+    return 'Power curently "{0}" waiting for "{1}", attempt {2} of {3}'.format( self.curent_state, self.desired_state, self.counter, MAX_POWER_SET_ATTEMPTS )
+
+  def rollback( self ):
+    self.counter = 0
+    self.curent_state = None
+
+  def toSubcontractor( self ):
+    self.counter += 1
+    if self.desired_state == 'off' and self.counter < 3:  # the first two times, do it nicely, after that, the hard way
+      return ( 'set_power', { 'state': 'soft_off', 'connection': self.connection_paramaters, 'vmid': self.vmid, 'name': self.name } )
+    else:
+      return ( 'set_power', { 'state': self.desired_state, 'connection': self.connection_paramaters, 'vmid': self.vmid, 'name': self.name } )
+
+  def fromSubcontractor( self, data ):
+    self.curent_state = data[ 'state' ]
+
+  def __getstate__( self ):
+    return ( self.connection_paramaters, self.vmid, self.name, self.desired_state, self.curent_state, self.counter )
+
+  def __setstate__( self, state ):
+    self.connection_paramaters = state[0]
+    self.vmid = state[1]
+    self.name = state[2]
+    self.desired_state = state[3]
+    self.curent_state = state[4]
+    self.counter = state[5]
+
+
+class power_state( ExternalFunction ):
+  def __init__( self, foundation, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self.vmid = foundation.proxmox_vmid
+    self.name = foundation.locator
+    self.connection_paramaters = foundation.proxmox_complex.connection_paramaters
+    self.state = None
+
+  @property
+  def done( self ):
+    return self.state is not None
+
+  @property
+  def message( self ):
+    if self.state is None:
+        return 'Retrieving for Power State'
+
+    return 'Power State at "{0}"'.format( self.state )
+
+  @property
+  def value( self ):
+    return self.state
+
+  def toSubcontractor( self ):
+    return ( 'power_state', { 'connection': self.connection_paramaters, 'vmid': self.vmid, 'name': self.name } )
+
+  def fromSubcontractor( self, data ):
+    self.state = data[ 'state' ]
+
+  def __getstate__( self ):
+    return ( self.connection_paramaters, self.vmid, self.name, self.state )
+
+  def __setstate__( self, state ):
+    self.connection_paramaters = state[0]
+    self.vmid = state[1]
+    self.name = state[3]
+    self.state = state[2]
+
+
+class wait_for_poweroff( ExternalFunction ):
+  def __init__( self, foundation, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self.vmid = foundation.proxmox_vmid
+    self.name = foundation.locator
+    self.connection_paramaters = foundation.proxmox_complex.connection_paramaters
+    self.current_state = None
+
+  @property
+  def done( self ):
+    return self.current_state == 'off'
+
+  @property
+  def message( self ):
+    return 'Waiting for Power off, curently "{0}"'.format( self.current_state )
+
+  def toSubcontractor( self ):
+    return ( 'power_state', { 'connection': self.connection_paramaters, 'vmid': self.vmid, 'name': self.name } )
+
+  def fromSubcontractor( self, data ):
+    self.current_state = data[ 'state' ]
+
+  def __getstate__( self ):
+    return ( self.connection_paramaters, self.vmid, self.name, self.current_state )
+
+  def __setstate__( self, state ):
+    self.connection_paramaters = state[0]
+    self.vmid = state[1]
+    self.name = state[2]
+    self.current_state = state[3]
+
+
+class get_interface_map( ExternalFunction ):
+  def __init__( self, foundation, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self.foundation = foundation
+    self.vmid = foundation.proxmox_vmid
+    self.name = foundation.locator
+    self.connection_paramaters = foundation.proxmox_complex.connection_paramaters
+    self.interface_list = None
+
+  @property
+  def done( self ):
+    return self.interface_list is not None
+
+  @property
+  def message( self ):
+    if self.interface_list is not None:
+      return 'Interface Map Length: "{0}"'.format( len( self.interface_list ) )
+    else:
+      return 'Waiting for Interface Map'
+
+  def setup( self, parms ):
+    pass
+
+  @property
+  def value( self ):
+    pos = 0
+    result = {}
+    for interface in self.foundation.networkinterface_set.all().order_by( 'physical_location' ):
+      result[ interface.physical_location ] = self.interface_list[ pos ]
+      pos += 1
+
+    return result
+
+  def toSubcontractor( self ):
+    return ( 'get_interface_map', { 'connection': self.connection_paramaters, 'vmid': self.vmid, 'name': self.name } )
+
+  def fromSubcontractor( self, data ):
+    self.interface_list = data[ 'interface_list' ]
+
+  def __getstate__( self ):
+    return ( self.foundation, self.connection_paramaters, self.vmid, self.name, self.interface_list )
+
+  def __setstate__( self, state ):
+    self.foundation = state[0]
+    self.connection_paramaters = state[1]
+    self.vmid = state[2]
+    self.name = state[3]
+    self.interface_list = state[4]
+
+
+class set_interface_macs():
+  def __init__( self, foundation, *args, **kwargs ):
+    super().__init__( *args, **kwargs )
+    self.foundation = foundation
+
+  def __call__( self, interface_map ):
+    for physical_location in interface_map:
+      try:
+        iface = self.foundation.networkinterface_set.get( physical_location=physical_location )
+      except ObjectDoesNotExist:
+        raise ParamaterError( 'interface_map', 'interface at "{0}" not found'.format( physical_location ) )
+
+      iface.mac = interface_map[ physical_location ]
+
+      try:
+        iface.full_clean()
+      except ValidationError as e:
+        raise ParamaterError( 'interface_map', 'Error saving interface "{0}": {1}'.format( physical_location, e ) )
+
+      iface.save()
+
+
+# plugin exports
+
+TSCRIPT_NAME = 'vcenter'
+
+TSCRIPT_FUNCTIONS = {
+                      'create': create,
+                      'node_list': node_list
+                    }
+
+TSCRIPT_VALUES = {
+                 }
